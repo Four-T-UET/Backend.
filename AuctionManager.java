a896443b4b882466a291638025ec0ea2f9e138db
Pham Minh Tuan <phammtuan23@gmail.com>
2026-04-11 20:20:32 +0700

    Singleton AuctionManager

--- AuctionManager.java
+++ AuctionManager.java
@@ -1,5 +1,16 @@
 import java.util.*;
 public class AuctionManager{
+	private static volatile AuctionManager instance;
+	public static AuctionManager getInstance(){
+		if (instance==null){
+			synchronized (AuctionManager.class){
+				if (instance==null){
+					instance=new AuctionManager();
+				}
+			}
+		}
+		return instance;
+	}
 	private HashMap<String,Auction> ds=new HashMap<>();
 	public void add(Auction auction){
 		this.ds.put(auction.getId(),auction);
